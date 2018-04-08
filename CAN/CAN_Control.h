5d3ac0c5b1979f7b4be6642122a07b41fa55b37c
Frederick Hsu(徐贊) <frederick.xu@amphenolmcp.com>
2018-04-09 01:22:57 +0800

    Add a new file "CAN_Control.h" under the CAN/ directory.
    In this CAN/ directory, these code will reserve to implement the CAN_Control module.

---
+++ CAN/CAN_Control.h
@@ -0,0 +1,15 @@
+/****************************************************************************************
+ * File name    : CAN_Control.h
+ * Description  : Configure some CAN settings in this CAN module.
+ * Creator      : Frederick Hsu
+ * Creation date: Sun.  09 April, 2018
+ * Copyright(C) 2018    All rights reserved.
+ *
+ ****************************************************************************************/
+
+#ifndef CAN_CONTROL_H
+#define CAN_CONTROL_H
+
+    int Set_CAN_BaudRate(char sBaudRateInfo[]);
+
+#endif  /* CAN_CONTROL_H */
