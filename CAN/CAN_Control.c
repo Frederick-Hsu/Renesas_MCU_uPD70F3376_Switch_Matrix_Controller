96069e102ca3508b94bbad016cc38b6c7e63950a
Frederick Hsu(徐贊) <frederick.xu@amphenolmcp.com>
2018-04-14 09:10:56 +0800

    Implement how to change the CAN baud rate, focus on 2 registers : C0BRP and C0BTR
    The 2 registers can be changed only in the initial mode.

--- CAN/CAN_Control.c
+++ CAN/CAN_Control.c
@@ -7,54 +7,95 @@
  *
  ****************************************************************************************/
 
-
+#include "../macrodriver.h"
+#include "../can.h"
 #include "CAN_Control.h"
 #include <string.h>
 
 #pragma ioreg
+
+MD_STATUS ChangeCanChBaudRate(SCHAR chno, UCHAR CnBRP, USHORT CnBTR)
+{
+	UCHAR	drvchno;
+	MD_STATUS status = MD_OK;
+    
+    UCHAR brp;
+    USHORT btr;
+
+	{
+		drvchno = (gcCanChIniUse[chno] & MSK_CH_DRVCHNO);	/* Set drvchno */
+		{
+			volatile CAN_ChRegType*	pChRegType;
+
+			/* Set CAN module register address */
+			pChRegType = (volatile CAN_ChRegType*)(gcCanRegBaseAddr[chno].pChRegBaseAddr);
+			/* Check init mode */
+			if ((pChRegType->CTRL & MSK_OPMODE) != CHK_INIT)
+			{
+				status = MD_ERROR;
+			}
+			else
+			{
+				/* Set baudrate */
+				// pChRegType->BRP = gcCanChIniBps[drvchno].CnBRP;
+                pChRegType->BRP = CnBRP;
+				// pChRegType->BTR = gcCanChIniBps[drvchno].CnBTR;
+                pChRegType->BTR = CnBTR;
+                
+                brp = C0BRP;
+                btr = C0BTR;
+			}
+		}
+	}
+
+	return (status);
+}
 
 int Set_CAN_BaudRate(char sBaudRateInfo[])
 {
     int iError = 0;
     if ((0 == strcmp(sBaudRateInfo, "1000KBPS")) || (0 == strcmp(sBaudRateInfo, "1000K")))
     {
-        C0BRP = 0x01;
-        C0BTR = 0x0706;
+        iError = CanChSetInitMode(0);
+        iError = ChangeCanChBaudRate(0, 0x01, 0x0706);
+        CAN0_SetNormalMode();
     }
     else if ((0 == strcmp(sBaudRateInfo, "500KBPS")) || (0 == strcmp(sBaudRateInfo, "500K")))
     {
-        C0BRP = 0x03;
-        C0BTR = 0x0706;
+        iError = CanChSetInitMode(0);
+        iError = ChangeCanChBaudRate(0, 0x03, 0x0706);
+        CAN0_SetNormalMode();
     }
     else if ((0 == strcmp(sBaudRateInfo, "250KBPS")) || (0 == strcmp(sBaudRateInfo, "250K")))
     {
-        C0BRP = 0x0F;
-        C0BTR = 0x0203;
+        iError = CanChSetInitMode(0);
+        iError = ChangeCanChBaudRate(0, 0x0F, 0x0203);
+        CAN0_SetNormalMode();
     }
     else if ((0 == strcmp(sBaudRateInfo, "125KBPS")) || (0 == strcmp(sBaudRateInfo, "125K")))
     {
-        C0BRP = 0x0F;
-        C0BTR = 0x0706;
+        iError = CanChSetInitMode(0);
+        iError = ChangeCanChBaudRate(0, 0x0F, 0x0706);
     }
     else if ((0 == strcmp(sBaudRateInfo, "100KBPS")) || (0 == strcmp(sBaudRateInfo, "100K")))
     {
-        C0BRP = 0x0F;
-        C0BTR = 0x070A;
+        iError = CanChEnable(0);
+        iError = ChangeCanChBaudRate(0, 0x0F, 0x070A);
     }
     else if ((0 == strcmp(sBaudRateInfo, "40KBPS")) || (0 == strcmp(sBaudRateInfo, "40K")))
     {
-        C0BRP = 0x063;
-        C0BTR = 0x0203;
+        iError = CanChEnable(0);
+        iError = ChangeCanChBaudRate(0, 0x63, 0x0203);
     }
     else if ((0 == strcmp(sBaudRateInfo, "20KBPS")) || (0 == strcmp(sBaudRateInfo, "20K")))
     {
-        C0BRP = 0x63;
-        C0BTR = 0x0706;
+        iError = CanChEnable(0);
+        iError = ChangeCanChBaudRate(0, 0x63, 0x0706);
     }
     else if ((0 == strcmp(sBaudRateInfo, "10KBPS")) || (0 == strcmp(sBaudRateInfo, "10K")))
     {
-        C0BRP = 0x7F;
-        C0BTR = 0x070F;
+        iError = CanChEnable(0);
+        iError = ChangeCanChBaudRate(0, 0x7F, 0x070F);
     }
     else
     {
